d8c22a0d5ca1afda483e68836d6415f0ddbf43e2
SalmanSakri <96472384+SalmanSakri@users.noreply.github.com>
2025-08-04 22:19:11 +0530

    deploy

--- src/hook/fetchApi.js
+++ src/hook/fetchApi.js
@@ -1,32 +1,40 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from "react";
 
 const useFetch = (url) => {
     const [data, setData] = useState([]);
     const [error, setError] = useState("");
     const [loading, setLoading] = useState(false);
+    const previousUrlRef = useRef(null);
 
     useEffect(() => {
+        if (!url) {
+            return;
+        }
+        // Don't refetch if URL hasn't changed
+        if (url === previousUrlRef.current) {
+            return;
+        }
+
         const fetchData = async () => {
-            if (!url) {
-                // console.log("Provied URL")
-            }
-            setLoading(true);
             try {
+                previousUrlRef.current = url;
+                setLoading(true);
                 const response = await fetch(url);
                 if (!response.ok) {
                     throw new Error(`HTTP error! status: ${response.status}`);
                 }
                 const json = await response.json();
-                // console.log(json)
+                // console.log("fetch data", json);
                 setData(json.results || json);
                 setLoading(false);
+
             } catch (err) {
                 setError(err.message);
                 // console.error("Fetch error:", err);
             }
         };
-
         fetchData();
+
     }, [url]);
 
     return { data, error, loading };
